992a026972a30b6ec60ce8f9226865eb35baa210
sgrzegorz <grzegorzsroka2@gmail.com>
2022-12-15 20:38:02 +0100

    done

--- wzory_czesciowe.m
+++ wzory_czesciowe.m
@@ -10,13 +10,13 @@ A11 = [0 0 1 0; 0 0 0 0; 0 -1 0 0; 0 0 0 1]
 A12 = [cos(theta1) -sin(theta1) 0 0; sin(theta1) cos(theta1) 0 0; 0 0 1 0; 0 0 0 1]
 A21 = [1 0 0 0; 0 cos(sym(pi)/2) -sin(sym(pi)/2) 0 ; 0 sin(sym(pi)/2) cos(sym(pi)/2) 0; 0 0 0 1]
 A22 = [cos(theta2) -sin(theta2) 0 a2*cos(theta2); sin(theta2) cos(theta2) 0 a2*sin(theta2); 0 0 1 0; 0 0 0 1]
-A3 = [cos(theta3), - sin(theta3), 0, a3*cos(theta3); sin(theta3), cos(theta3), 0, a3*sin(theta3); 0 , 0, 1,0; 0,0,0,1];
+A3 = [cos(theta3), - sin(theta3), 0, a3*cos(theta3); sin(theta3), cos(theta3), 0, a3*sin(theta3); 0 , 0, 1,0; 0,0,0,1]
 
-A4 = [cos(theta4), -sin(theta4), 0, a4*cos(theta4); sin(theta4), cos(theta4), 0, a4*sin(theta4); 0 0 1 0 ; 0  0 0 1 ];
+A4 = [cos(theta4), -sin(theta4), 0, a4*cos(theta4); sin(theta4), cos(theta4), 0, a4*sin(theta4); 0 0 1 0 ; 0  0 0 1 ]
 
-A51 = [1 0 0 0 ; 0 cos(sym(pi)/2) -sin(sym(pi)/2) 0; 0 sin(sym(pi)/2) cos(sym(pi)/2) 0; 0 0 0 1];
+A51 = [1 0 0 0 ; 0 cos(sym(pi)/2) -sin(sym(pi)/2) 0; 0 sin(sym(pi)/2) cos(sym(pi)/2) 0; 0 0 0 1]
 
-A52 = [cos(theta5), -sin(theta5), 0, a5*cos(theta5); sin(theta5) cos(theta5) 0 a5*sin(theta5); 0 0 1 0; 0 0 0 1];
+A52 = [cos(theta5), -sin(theta5), 0, a5*cos(theta5); sin(theta5) cos(theta5) 0 a5*sin(theta5); 0 0 1 0; 0 0 0 1]
 
 A61 = [0,0,-1 0; 0 0 0 0; 0 1 0 0; 0 0 0 1];
 
